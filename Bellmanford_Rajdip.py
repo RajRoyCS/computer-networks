687407b956e6bd0ad4a57160147ffa83b390c646
RajRoyCS <rajroy26451003@gmail.com>
2026-09-07 21:37:06 +0530

    improve bellmanford implementation

--- Bellmanford_Rajdip.py
+++ Bellmanford_Rajdip.py
@@ -1,35 +1,50 @@
-def bellman_ford(graph, V, source):
+def bellman_ford(V, graph, source):
 
-    distance = [9999] * V
+    INF = 9999
+    distance = [INF] * V
     distance[source] = 0
 
-    # Relax edges V-1 times
+    # Relax all edges V-1 times
     for i in range(V - 1):
+        updated = False
+
         for u, v, w in graph:
-            if distance[u] != 9999 and distance[u] + w < distance[v]:
+            if distance[u] != INF and distance[u] + w < distance[v]:
                 distance[v] = distance[u] + w
+                updated = True
+
+        # Stop early if no distance was updated
+        if not updated:
+            break
 
     # Check for negative weight cycle
     for u, v, w in graph:
-        if distance[u] != 9999 and distance[u] + w < distance[v]:
-            print("Negative weight cycle exists")
+        if distance[u] != INF and distance[u] + w < distance[v]:
+            print("\nNegative weight cycle exists!")
             return
 
-    print("Vertex\tDistance")
+    # Display shortest distances
+    print("\nVertex\tDistance")
 
     for i in range(V):
-        print(i, "\t", distance[i])
+        if distance[i] == INF:
+            print(i, "\tINF")
+        else:
+            print(i, "\t", distance[i])
 
 
-graph = [
-    (0, 1, 4),
-    (0, 2, 5),
-    (1, 2, -3),
-    (2, 3, 4),
-    (3, 1, -2)
-]
+# Taking input from user
+V = int(input("Enter number of vertices: "))
+E = int(input("Enter number of edges: "))
 
-V = 4
-source = 0
+graph = []
 
-bellman_ford(graph, V, source)
+print("\nEnter edges in the format: source destination weight")
+
+for i in range(E):
+    u, v, w = map(int, input(f"Edge {i + 1}: ").split())
+    graph.append((u, v, w))
+
+source = int(input("\nEnter source vertex: "))
+
+bellman_ford(V, graph, source)
